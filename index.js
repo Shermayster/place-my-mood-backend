de0db65a7b19c7c65bf51c1e85573edd4ca05d60
pavelshermayster <sherm84@gmail.com>
2018-07-27 11:11:18 +0300

    feat: body perser and root added bodyParser for logging and root response

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.send('root');
+})
 
 app.listen(2000);
